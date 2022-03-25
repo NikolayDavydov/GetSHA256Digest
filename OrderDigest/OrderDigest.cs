23e8c4cce016ed2fd7c172e92a3c6f52e54fe32b
Nikolay <nikolich253@icloud.com>
2022-03-25 09:55:41 +0300

    release

---
+++ OrderDigest/OrderDigest.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace OrderDigest
+{
+    public static class OrderDigest
+    {
+        public static string ComputeHash(string value)
+        {
+            var newVal = Encoding.ASCII.GetBytes(value);
+            SHA256Managed sha256m = new SHA256Managed();
+            return Encoding.ASCII.GetString(sha256m.ComputeHash(newVal));
+        }
+    }
+}
